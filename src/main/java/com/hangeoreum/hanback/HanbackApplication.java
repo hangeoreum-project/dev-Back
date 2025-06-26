131bb129f6968db849a989be999597787966d511
suwdle <suwdle1917@gmail.com>
2025-06-27 01:59:02 +0900

    initialize gradle project

---
+++ src/main/java/com/hangeoreum/hanback/HanbackApplication.java
@@ -0,0 +1,13 @@
+package com.hangeoreum.hanback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangeoreum/hanback/HanbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangeoreum.hanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
