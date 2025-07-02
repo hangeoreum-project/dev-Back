d8fcfa636bd67a78f52e8a1119d072348fa9b38c
suwdle <suwdle1917@gmail.com>
2025-07-02 16:51:04 +0900

    UPDATE JwtTokenProvider.java:jwt sign 구버전 삭제

--- src/main/java/com/hangeoreum/hanback/auth/jwt/JwtTokenProvider.java
+++ src/main/java/com/hangeoreum/hanback/auth/jwt/JwtTokenProvider.java
@@ -2,16 +2,22 @@ package com.hangeoreum.hanback.auth.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
 import java.util.Date;
 
 public class JwtTokenProvider {
+
+    private static final String SECRET = "mySecretKeyMustBeAtLeast32Chars!!";
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
 
     public static String generateToken(Long userId) {
         return Jwts.builder()
                 .setSubject(String.valueOf(userId))
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS256, JwtProperties.SECRET)
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1시간
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 }
